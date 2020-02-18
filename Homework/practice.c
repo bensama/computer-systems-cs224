8551e764d7166498080086b6ad603802fa84ab7e
Ben-SSH-Remote <bentppaine@gmail.com>
2020-02-18 14:04:43 -0700

    finish HW1

--- Homework/practice.c
+++ Homework/practice.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
-#define SIZE 4
-int main()
-{
-    int a[SIZE];
-    char *s_ptr = (char *)(a + 1);
-    scanf("%x", a);
-    scanf("%s", s_ptr);
-    scanf("%x", &a[3]);
-    printf("%s", (char *)a);
+
+#define SIZE 24
+
+int main() {
+    char str[SIZE];
+    long *u_ptr = (long*)str;
+    int *i_ptr = (int *)(u_ptr + 1);
+    char *c_ptr = (char *)(i_ptr + 2);
+
+    scanf("%lx %x %x %s", u_ptr, i_ptr, i_ptr + 1, c_ptr);
+
+    printf("str = %s\n", str);  
+
     return 0;
 }
+//str = I <3 cs 224!! (*ˆ_ˆ*)
