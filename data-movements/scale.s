3cf4491e443f7202dbb9c8c62f195c0b753efaf6
Ben <bentppain@gmail.com>
2020-03-11 15:27:10 -0600

    data

---
+++ data-movements/scale.s
@@ -0,0 +1,29 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 15	sdk_version 10, 15
+	.globl	_scale                  ## -- Begin function scale
+	.p2align	4, 0x90
+_scale:                                 ## @scale
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	movq	%rdi, -8(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	%rdx, -24(%rbp)
+	movq	-8(%rbp), %rdx
+	movq	-16(%rbp), %rsi
+	shlq	$2, %rsi
+	addq	%rsi, %rdx
+	imulq	$12, -24(%rbp), %rsi
+	addq	%rsi, %rdx
+	movq	%rdx, -32(%rbp)
+	movq	-32(%rbp), %rax
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+
+.subsections_via_symbols
--- practice/recursion.c
+++ practice/recursion.c
@@ -3,19 +3,21 @@
 int array[] = {1,2,3};
 
 int main() {
-    int *rdx = array;
+    int *rdi = array;
     int rsi  = 3;
     recursive_sum(rdx, rsi);
 }
 
-int recursive_sum(int *rdx, int rsi) {
+int recursive_sum(int *rdi, int rsi) {
     int eax = 0;        // xorq %eax, %eax
     if(rsi == 0) {      // andq %rsi, %rsi
         goto done;      // je done
     }
 
-    eax = recursive_sum(rdx + 1, rsi - 1) + *rdx;
-
+    rdi = rdi + 1;
+    rsi = rsi - 1;
+    eax = recursive_sum(rdi + 1, rsi - 1);
+    eax = eax + *rdi;
 done:
     return eax;
 }
